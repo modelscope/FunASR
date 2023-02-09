e4b072790e0f3b8a9b5e2689907f03a2421208c4
onlybetheone <iriszhangchong@gmail.com>
2023-02-09 11:52:14 +0800

    add persian uniasr online & offline model finetune & infer scripts

---
+++ egs_modelscope/asr/uniasr/speech_UniASR_asr_2pass-fa-16k-common-vocab1257-pytorch-offline/infer.py
@@ -0,0 +1,13 @@
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+
+if __name__ == "__main__":
+    audio_in = "https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_fa.wav"
+    output_dir = "./results"
+    inference_pipline = pipeline(
+        task=Tasks.auto_speech_recognition,
+        model="damo/speech_UniASR_asr_2pass-fa-16k-common-vocab1257-pytorch-offline",
+        output_dir=output_dir,
+    )
+    rec_result = inference_pipline(audio_in=audio_in)
+    print(rec_result)
