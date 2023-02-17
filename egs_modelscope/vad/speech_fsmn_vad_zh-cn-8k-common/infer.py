861f2f26056aea26da1a9eedc2f0288e07254ec2
lzr265946 <lzr265946@alibaba-inc.com>
2023-02-17 11:12:26 +0800

    fix demo in egs_modelscope vad

--- egs_modelscope/vad/speech_fsmn_vad_zh-cn-8k-common/infer.py
+++ egs_modelscope/vad/speech_fsmn_vad_zh-cn-8k-common/infer.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     inference_pipline = pipeline(
         task=Tasks.voice_activity_detection,
         model="damo/speech_fsmn_vad_zh-cn-8k-common",
-        model_revision='v1.1.1',
+        model_revision=None,
         output_dir='./output_dir',
         batch_size=1,
     )
