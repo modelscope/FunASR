add6848ba5d747fa9f20b205d434403a1ee0eebf
SWHL <liekkaskono@163.com>
2023-02-21 15:45:41 +0800

    Fix README path error

--- funasr/runtime/python/onnxruntime/setup.py
+++ funasr/runtime/python/onnxruntime/setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 def get_readme():
     root_dir = Path(__file__).resolve().parent
-    readme_path = str(root_dir / 'rapid_paraformer' / 'README.md')
+    readme_path = str(root_dir / 'README.md')
     print(readme_path)
     with open(readme_path, 'r', encoding='utf-8') as f:
         readme = f.read()
