c0b48ccf6af5d8edf0a0912564257a08eab4fd4b
speech_asr <wangjiaming.wjm@alibaba-inc.com>
2023-03-10 17:39:22 +0800

    update unittest

--- tests/test_asr_inference_pipeline.py
+++ tests/test_asr_inference_pipeline.py
@@ -313,6 +313,24 @@ class TestUniasrInferencePipelines(unittest.TestCase):
             audio_in='https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_id.wav',
             param_dict={"decoding_model":"normal"})
         logger.info("asr inference result: {0}".format(rec_result))
+        
+    def test_uniasr_2pass_ja_common_offline(self):
+        inference_pipeline = pipeline(
+            task=Tasks.auto_speech_recognition,
+            model='damo/speech_UniASR_asr_2pass-ja-16k-common-vocab93-tensorflow1-offline')
+        rec_result = inference_pipeline(
+            audio_in='https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_ja.wav',
+            param_dict={"decoding_model":"offline"})
+        logger.info("asr inference result: {0}".format(rec_result))
+
+    def test_uniasr_2pass_ja_common_online(self):
+        inference_pipeline = pipeline(
+            task=Tasks.auto_speech_recognition,
+            model='damo/speech_UniASR_asr_2pass-ja-16k-common-vocab93-tensorflow1-online')
+        rec_result = inference_pipeline(
+            audio_in='https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_ja.wav',
+            param_dict={"decoding_model":"normal"})
+        logger.info("asr inference result: {0}".format(rec_result))
 
 
 if __name__ == '__main__':
