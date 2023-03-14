1e4eba6a72ea97d9a9e733df3e3b1eb86e4fd44d
speech_asr <wangjiaming.wjm@alibaba-inc.com>
2023-03-14 23:22:31 +0800

    update

--- egs_modelscope/speaker_diarization/speech_diarization_eend-ola-en-us-callhome-8k/infer.py
+++ egs_modelscope/speaker_diarization/speech_diarization_eend-ola-en-us-callhome-8k/infer.py
@@ -4,5 +4,6 @@ from modelscope.utils.constant import Tasks
 inference_diar_pipline = pipeline(
     task=Tasks.speaker_diarization,
     model='damo/speech_diarization_eend-ola-en-us-callhome-8k',
+    model_revision="v1.0.0",
 )
 results = inference_diar_pipline(audio_in="https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_data/record.wav")
