9232f066040c909f79eb7eeb70d5a3a0e1b97b4f
九耳 <mengzhe.cmz@alibaba-inc.com>
2023-03-29 17:01:26 +0800

    general punc model conversion onnx, fix bug

--- funasr/export/models/__init__.py
+++ funasr/export/models/__init__.py
@@ -3,6 +3,7 @@ from funasr.export.models.e2e_asr_paraformer import Paraformer as Paraformer_exp
 from funasr.export.models.e2e_asr_paraformer import BiCifParaformer as BiCifParaformer_export
 from funasr.models.e2e_vad import E2EVadModel
 from funasr.export.models.e2e_vad import E2EVadModel as E2EVadModel_export
+from funasr.export.models.target_delay_transformer import TargetDelayTransformer as TargetDelayTransformer_export
 
 def get_model(model, export_config=None):
     if isinstance(model, BiCifParaformer):
@@ -11,5 +12,8 @@ def get_model(model, export_config=None):
         return Paraformer_export(model, **export_config)
     elif isinstance(model, E2EVadModel):
         return E2EVadModel_export(model, **export_config)
+    elif isinstance(model, ESPnetPunctuationModel):
+        if isinstance(model.punc_model, TargetDelayTransformer):
+            return TargetDelayTransformer_export(model.punc_model, **export_config)
     else:
         raise "Funasr does not support the given model type currently."
