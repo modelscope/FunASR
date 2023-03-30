e55178abc21a3a692b7b18cc12922b4004c15f2e
九耳 <mengzhe.cmz@alibaba-inc.com>
2023-03-30 14:11:02 +0800

    general punc model runtime

--- funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
+++ funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
@@ -1,3 +1,5 @@
 # -*- encoding: utf-8 -*-
 from .paraformer_bin import Paraformer
 from .vad_bin import Fsmn_vad
+from .punc_bin import TargetDelayTransformer
+#from .punc_bin import VadRealtimeTransformer
--- funasr/runtime/python/onnxruntime/funasr_onnx/utils/utils.py
+++ funasr/runtime/python/onnxruntime/funasr_onnx/utils/utils.py
@@ -215,6 +215,19 @@ class OrtInferSession():
         if not model_path.is_file():
             raise FileExistsError(f'{model_path} is not a file.')
 
+def split_to_mini_sentence(words: list, word_limit: int = 20):
+    assert word_limit > 1
+    if len(words) <= word_limit:
+        return [words]
+    sentences = []
+    length = len(words)
+    sentence_len = length // word_limit
+    for i in range(sentence_len):
+        sentences.append(words[i * word_limit:(i + 1) * word_limit])
+    if length % word_limit > 0:
+        sentences.append(words[sentence_len * word_limit:])
+    return sentences
+
 
 def read_yaml(yaml_path: Union[str, Path]) -> Dict:
     if not Path(yaml_path).exists():
