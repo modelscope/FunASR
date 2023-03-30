3cd71a385a31f987f2db99df902ca36ee02b1813
九耳 <mengzhe.cmz@alibaba-inc.com>
2023-03-30 17:29:12 +0800

    change name

--- funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
+++ funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
 from .paraformer_bin import Paraformer
 from .vad_bin import Fsmn_vad
-from .punc_bin import CT-Transformer
-#from .punc_bin import VadRealtimeCT-Transformer
+from .punc_bin import CT_Transformer
+#from .punc_bin import VadRealtimeCT_Transformer
