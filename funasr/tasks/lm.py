4ba1011b42e041ee1d71448eefd7ef2e7bd61bb6
游雁 <zhifu.gzf@alibaba-inc.com>
2023-03-31 15:31:26 +0800

    export

--- funasr/tasks/lm.py
+++ funasr/tasks/lm.py
@@ -15,7 +15,7 @@ from typeguard import check_return_type
 from funasr.datasets.collate_fn import CommonCollateFn
 from funasr.datasets.preprocessor import CommonPreprocessor
 from funasr.lm.abs_model import AbsLM
-from funasr.lm.espnet_model import LanguageModel
+from funasr.lm.abs_model import LanguageModel
 from funasr.lm.seq_rnn_lm import SequentialRNNLM
 from funasr.lm.transformer_lm import TransformerLM
 from funasr.tasks.abs_task import AbsTask
