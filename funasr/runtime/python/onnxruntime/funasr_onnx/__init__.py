79007d36f1636eb51e0cead6bc0e6b18ff1f8253
九耳 <mengzhe.cmz@alibaba-inc.com>
2023-04-07 14:43:32 +0800

    vad_realtime onnx runnable

--- funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
+++ funasr/runtime/python/onnxruntime/funasr_onnx/__init__.py
@@ -2,4 +2,4 @@
 from .paraformer_bin import Paraformer
 from .vad_bin import Fsmn_vad
 from .punc_bin import CT_Transformer
-#from .punc_bin import VadRealtimeCT_Transformer
+from .punc_bin import CT_Transformer_VadRealtime
