52eb056c76f80e1bc8e77867de8922bb6a1c41c6
speech_asr <wangjiaming.wjm@alibaba-inc.com>
2023-04-21 01:29:44 +0800

    update

--- funasr/build_utils/build_model.py
+++ funasr/build_utils/build_model.py
@@ -3,6 +3,7 @@ from funasr.build_utils.build_lm_model import build_lm_model
 from funasr.build_utils.build_pretrain_model import build_pretrain_model
 from funasr.build_utils.build_punc_model import build_punc_model
 from funasr.build_utils.build_vad_model import build_vad_model
+from funasr.build_utils.build_diar_model import build_diar_model
 
 
 def build_model(args):
@@ -16,6 +17,8 @@ def build_model(args):
         model = build_punc_model(args)
     elif args.task_name == "vad":
         model = build_vad_model(args)
+    elif args.task_name == "diar":
+        model = build_diar_model(args)
     else:
         raise NotImplementedError("Not supported task: {}".format(args.task_name))
 
