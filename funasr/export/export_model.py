d407c912dc3d9d64ad32b85bba922299ed5abb47
吴伟鹏 <2662345338@qq.com>
2023-05-09 07:25:49 +0000

    add the uonnx of uniasr

--- funasr/export/export_model.py
+++ funasr/export/export_model.py
@@ -184,6 +184,15 @@ class ModelExport:
                     mode = config_data['model']['punc_model_config']['mode']
                 else:
                     mode = config_data['model']['model_config']['mode']
+        if mode.startswith('uniasr'):
+            from funasr.tasks.asr import ASRTaskParaformer as ASRTask
+            config = os.path.join(model_dir, 'config.yaml')
+            model_file = os.path.join(model_dir, 'model.pb')
+            cmvn_file = os.path.join(model_dir, 'am.mvn')
+            model, asr_train_args = ASRTask.build_model_from_file(
+                config, model_file, cmvn_file, 'cpu'
+            )
+            self.frontend = model.frontend
         if mode.startswith('paraformer'):
             from funasr.tasks.asr import ASRTaskParaformer as ASRTask
             config = os.path.join(model_dir, 'config.yaml')
