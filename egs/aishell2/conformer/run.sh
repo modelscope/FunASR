6a4a8ba0d5e5adba8b4bb5c957ab3d7c914686a3
嘉渊 <wangjiaming.wjm@alibaba-inc.com>
2023-05-12 17:29:12 +0800

    update repo

--- egs/aishell2/conformer/run.sh
+++ egs/aishell2/conformer/run.sh
@@ -9,7 +9,7 @@ count=1
 gpu_inference=true  # Whether to perform gpu decoding, set false for cpu decoding
 # for gpu decoding, inference_nj=ngpu*njob; for cpu decoding, inference_nj=njob
 njob=5
-train_cmd=tools/run.pl
+train_cmd=utils/run.pl
 infer_cmd=utils/run.pl
 
 # general configuration
