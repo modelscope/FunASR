cb7c35d9a6d9883afd6eb1995840f1f4d7a182fa
嘉渊 <wangjiaming.wjm@alibaba-inc.com>
2023-05-12 16:55:04 +0800

    update repo

--- egs/aishell2/conformer/run.sh
+++ egs/aishell2/conformer/run.sh
@@ -29,7 +29,7 @@ nj=64
 
 # data
 tr_dir=/nfs/wangjiaming.wjm/asr_data/aishell2/AISHELL-2/iOS/data
-dev_tst_dir=/nfs/wangjiaming.wjm/asr_data/aishell2/AISHELL-DEV-TEST-SET
+dev_tst_dir=/nfs/wangjiaming.wjm/asr_data/aishell2/AISHELL-DEV-TEST-SET/iOS
 
 # exp tag
 tag="exp1"
