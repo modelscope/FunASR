0c816d1995847a157d7bceac20ffef35bced438a
嘉渊 <wangjiaming.wjm@alibaba-inc.com>
2023-05-15 14:24:42 +0800

    update repo

--- egs/aishell/paraformerbert/run.sh
+++ egs/aishell/paraformerbert/run.sh
@@ -139,6 +139,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
                 --data_dir ${feats_dir}/data \
                 --train_set ${train_set} \
                 --valid_set ${valid_set} \
+                --embed_path ${feats_dir}/data \
                 --cmvn_file ${feats_dir}/data/${train_set}/cmvn/cmvn.mvn \
                 --speed_perturb ${speed_perturb} \
                 --resume true \
