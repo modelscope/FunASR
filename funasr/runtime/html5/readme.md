2ec6637c8d2e3c514008dc7609112420e32f6b5e
zhaomingwork <zhaomingwork@qq.com>
2023-05-15 05:13:14 +0000

    add html5 demo for online ws srv

---
+++ funasr/runtime/html5/readme.md
@@ -0,0 +1,91 @@
+# online asr demo for html5
+
+## requirement
+### python
+```shell
+flask
+gevent
+pyOpenSSL
+```
+
+### javascript
+```shell
+Recorder [html5录音](https://github.com/xiangyuecn/Recorder)
+```
+## html5服务配置
+### 启动html5服务
+
+```shell
+usage: h5Server.py [-h] [--host HOST] [--port PORT] [--certfile CERTFILE]
+                   [--keyfile KEYFILE]
+python h5Server.py --port 1337
+```
+注:
+wsconnecter.js里配置online asr wss路径
+var Uri = "wss://xxx:xxx/" 
+
+### 浏览器打开地址
+https://127.0.0.1:1337/static/index.html
+
+
+### demo页面如下
+![img](https://github.com/zhaomingwork/FunASR/tree/for-html5-demo/funasr/runtime/html5/demo.gif)
+
+
+##后端配置
+h5打开麦克风需要https协议，同时后端的asr websocket也必须是wss协议，而目前[online asr](https://github.com/alibaba-damo-academy/FunASR/tree/main/funasr/runtime/python/websocket)模型只支持ws协议，所以我们通过nginx配置实现wss协议到ws协议的转换。
+
+##具体过程如下：
+浏览器htts --> html5 demo服务 --> js wss接口 --> nginx服务 --> ws asr online srv
+
+##配置nginx wss协议(了解的可以跳过）
+生成证书(注意这种证书并不能被所有浏览器认可，部分手动授权可以访问,最好使用其他认证的官方ssl证书)
+
+### 生成私钥，按照提示填写内容
+openssl genrsa -des3 -out server.key 1024
+ 
+### 生成csr文件 ，按照提示填写内容
+openssl req -new -key server.key -out server.csr
+ 
+### 去掉pass
+cp server.key server.key.org 
+openssl rsa -in server.key.org -out server.key
+ 
+### 生成crt文件，有效期1年（365天）
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+##nginx转发配置示例
+```shell
+events {                                                                                                            [0/1548]
+    worker_connections  1024;
+    accept_mutex on;
+  }
+http {
+  error_log  error.log;
+  access_log  access.log;
+  server {
+
+    listen 5921 ssl http2;  # nginx listen port for wss
+    server_name www.test.com;
+
+    ssl_certificate     /funasr/server.crt;
+    ssl_certificate_key /funasr/server.key;
+    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers         HIGH:!aNULL:!MD5;
+
+    location /wss/ {
+
+
+      proxy_pass http://127.0.0.1:1111/;  # asr online model ws address:port
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_read_timeout 600s;
+
+    }
+  }
+```
+
+## Acknowledge
+1. This project is maintained by [FunASR community](https://github.com/alibaba-damo-academy/FunASR).
+2. We acknowledge [爱医声](http://www.aihealthx.com/) for contributing the html5 demo.
---
+++ funasr/runtime/html5/static/main.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright FunASR (https://github.com/alibaba-damo-academy/FunASR). All Rights
+ * Reserved. MIT License  (https://opensource.org/licenses/MIT)
+ */
+/* 2022-2023 by zhaoming,mali aihealthx.com */
+
+
+// 连接; 定义socket连接类对象与语音对象
+var wsconnecter = new WebSocketConnectMethod({msgHandle:getJsonMessage,stateHandle:getConnState});
+var audioBlob;
+
+// 录音; 定义录音对象,wav格式
+var rec = Recorder({
+	type:"pcm",
+	bitRate:16,
+	sampleRate:16000,
+	onProcess:recProcess
+});
+
+ 
+ 
+ 
+var sampleBuf=new Int16Array();
+// 定义按钮响应事件
+var btnStart = document.getElementById('btnStart');
+btnStart.onclick = start;
+var btnStop = document.getElementById('btnStop');
+btnStop.onclick = stop;
+btnStop.disabled = true;
+ 
+
+ 
+var rec_text=""
+var info_div = document.getElementById('info_div');
+ 
+
+// 语音识别结果; 对jsonMsg数据解析,将识别结果附加到编辑框中
+function getJsonMessage( jsonMsg ) {
+	console.log( "message: " + JSON.parse(jsonMsg.data)['text'] );
+	var rectxt=""+JSON.parse(jsonMsg.data)['text'];
+	var varArea=document.getElementById('varArea');
+	rec_text=rec_text+rectxt.replace(/ +/g,"");
+	varArea.value=rec_text;
+	 
+ 
+}
+
+// 连接状态响应
+function getConnState( connState ) {
+	if ( connState === 0 ) {
+ 
+		rec.open( function(){
+			rec.start();
+			console.log("开始录音");
+ 
+		});
+	} else if ( connState === 1 ) {
+		//stop();
+	} else if ( connState === 2 ) {
+		stop();
+		console.log( 'connecttion error' );
+	}
+}
+
+
+// 识别启动、停止、清空操作
+function start() {
+	info_div.innerHTML="正在连接asr服务器，请等待...";
+	// 清除显示
+	clear();
+	//控件状态更新
+ 	    
+	isRec = true;
+	btnStart.disabled = true;
+	btnStop.disabled = false;
+	//启动连接
+	wsconnecter.wsStart();
+}
+
+ 
+function stop() {
+		var chunk_size = new Array( 5, 10, 5 );
+		var request = {
+			"chunk_size": chunk_size,
+			"wav_name":  "h5",
+			"is_speaking":  false,
+			"chunk_interval":10,
+		};
+		if(sampleBuf.length>0){
+		wsconnecter.wsSend(sampleBuf,false);
+		console.log("sampleBuf.length"+sampleBuf.length);
+		sampleBuf=new Int16Array();
+		}
+	   wsconnecter.wsSend( JSON.stringify(request) ,false);
+ 
+	 
+	
+	
+
+	
+	// 控件状态更新
+	isRec = false;
+    info_div.innerHTML="请等候...";
+	btnStop.disabled = true;
+	setTimeout(function(){btnStart.disabled = false;info_div.innerHTML="请点击开始";}, 3000 );
+	rec.stop(function(blob,duration){
+  
+		console.log(blob);
+		var audioBlob = Recorder.pcm2wav(data = {sampleRate:16000, bitRate:16, blob:blob},
+		function(theblob,duration){
+				console.log(theblob);
+		var audio_record = document.getElementById('audio_record');
+		audio_record.src =  (window.URL||webkitURL).createObjectURL(theblob); 
+        audio_record.controls=true;
+		audio_record.play(); 
+         	
+
+	}   ,function(msg){
+		 console.log(msg);
+	}
+		);
+ 
+
+ 
+	},function(errMsg){
+		console.log("errMsg: " + errMsg);
+	});
+    // 停止连接
+	
+    
+
+}
+
+function clear() {
+ 
+    var varArea=document.getElementById('varArea');
+ 
+	varArea.value="";
+    rec_text="";
+ 
+}
+
+ 
+function recProcess( buffer, powerLevel, bufferDuration, bufferSampleRate,newBufferIdx,asyncEnd ) {
+	if ( isRec === true ) {
+		var data_48k = buffer[buffer.length-1];  
+ 
+		var  array_48k = new Array(data_48k);
+		var data_16k=Recorder.SampleData(array_48k,bufferSampleRate,16000).data;
+ 
+		sampleBuf = Int16Array.from([...sampleBuf, ...data_16k]);
+		var chunk_size=960; // for asr chunk_size [5, 10, 5]
+		info_div.innerHTML=""+bufferDuration/1000+"s";
+		while(sampleBuf.length>=chunk_size){
+		    sendBuf=sampleBuf.slice(0,chunk_size);
+			sampleBuf=sampleBuf.slice(chunk_size,sampleBuf.length);
+			wsconnecter.wsSend(sendBuf,false);
+			
+			
+		 
+		}
+		
+ 
+		
+	}
+}
