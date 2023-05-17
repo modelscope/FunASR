12deb6b77bd1a98f7099514fb4d29d70fa90f4be
嘉渊 <wangjiaming.wjm@alibaba-inc.com>
2023-05-17 18:47:44 +0800

    update repo

--- egs/aishell2/paraformerbert/run.sh
+++ egs/aishell2/paraformerbert/run.sh
@@ -147,7 +147,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
                 --data_dir ${feats_dir}/data \
                 --train_set ${train_set} \
                 --valid_set ${valid_set} \
-                --data_file_names "wav.scp,text,embed.scp" \
+                --data_file_names "wav.scp,text,embeds.scp" \
                 --cmvn_file ${feats_dir}/data/${train_set}/cmvn/cmvn.mvn \
                 --speed_perturb ${speed_perturb} \
                 --dataset_type $dataset_type \
