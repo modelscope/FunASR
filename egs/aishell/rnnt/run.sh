21e8e30a75c11ede94fa71121d587d37224dcca4
aky15 <ankeyu.aky@11.17.44.249>
2023-06-02 15:32:35 +0800

    bug fix

--- egs/aishell/rnnt/run.sh
+++ egs/aishell/rnnt/run.sh
@@ -4,7 +4,7 @@
 
 # machines configuration
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-gpu_num=2
+gpu_num=8
 count=1
 gpu_inference=true  # Whether to perform gpu decoding, set false for cpu decoding
 # for gpu decoding, inference_nj=ngpu*njob; for cpu decoding, inference_nj=njob
