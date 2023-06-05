33784c4eef6295c8aa97324f165270be5906af53
haoneng.lhn <haoneng.lhn@alibaba-inc.com>
2023-06-05 16:25:25 +0800

    update paraformer online recipe

--- egs_modelscope/asr/paraformer/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/demo.py
+++ egs_modelscope/asr/paraformer/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online/demo.py
@@ -5,7 +5,7 @@ inference_pipeline = pipeline(
     task=Tasks.auto_speech_recognition,
     model='damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online',
     model_revision='v1.0.6',
-    update_model='v1.0.6',
+    update_model=False,
     mode="paraformer_fake_streaming"
 )
 audio_in='https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_zh.wav'
