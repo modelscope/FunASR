c2a2575f198b1bfd452ea5769bec81bcce3d3a42
manyeyes <32889020+manyeyes@users.noreply.github.com>
2023-06-21 09:31:59 +0800

    add c# assembly for fsmn vad (#650)
    Co-authored-by: zx <12345678>

---
+++ funasr/runtime/csharp/AliFsmnVadSharp/Model/FrontendConfEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliFsmnVadSharp.Model
+{
+    public class FrontendConfEntity
+    {
+        private int _fs = 16000;
+        private string _window = "hamming";
+        private int _n_mels = 80;
+        private int _frame_length = 25;
+        private int _frame_shift = 10;
+        private float _dither = 0.0F;
+        private int _lfr_m = 5;
+        private int _lfr_n = 1;
+
+        public int fs { get => _fs; set => _fs = value; }
+        public string window { get => _window; set => _window = value; }
+        public int n_mels { get => _n_mels; set => _n_mels = value; }
+        public int frame_length { get => _frame_length; set => _frame_length = value; }
+        public int frame_shift { get => _frame_shift; set => _frame_shift = value; }
+        public float dither { get => _dither; set => _dither = value; }
+        public int lfr_m { get => _lfr_m; set => _lfr_m = value; }
+        public int lfr_n { get => _lfr_n; set => _lfr_n = value; }
+    }
+}
---
+++ funasr/runtime/csharp/AliFsmnVadSharp/Model/VadYamlEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliFsmnVadSharp.Model
+{
+    internal class VadYamlEntity
+    {
+        private int _input_size;
+        private string _frontend = "wav_frontend";
+        private FrontendConfEntity _frontend_conf=new FrontendConfEntity();
+        private string _model = "e2evad";
+        private string _encoder = "fsmn";
+        private EncoderConfEntity _encoder_conf=new EncoderConfEntity();
+        private VadPostConfEntity _vad_post_conf=new VadPostConfEntity();
+
+        public int input_size { get => _input_size; set => _input_size = value; }
+        public string frontend { get => _frontend; set => _frontend = value; }
+        public string model { get => _model; set => _model = value; }
+        public string encoder { get => _encoder; set => _encoder = value; }
+        public FrontendConfEntity frontend_conf { get => _frontend_conf; set => _frontend_conf = value; }
+        public EncoderConfEntity encoder_conf { get => _encoder_conf; set => _encoder_conf = value; }
+        public VadPostConfEntity vad_post_conf { get => _vad_post_conf; set => _vad_post_conf = value; }
+    }
+}
