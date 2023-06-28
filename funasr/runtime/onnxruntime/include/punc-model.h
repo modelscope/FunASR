db376fbd8bc2ba66b56a2b66d9907bd58aa198d6
雾聪 <wucong.lyb@alibaba-inc.com>
2023-06-28 10:58:24 +0800

    update punc-model.h

--- funasr/runtime/onnxruntime/include/punc-model.h
+++ funasr/runtime/onnxruntime/include/punc-model.h
@@ -12,8 +12,8 @@ class PuncModel {
   public:
     virtual ~PuncModel(){};
 	  virtual void InitPunc(const std::string &punc_model, const std::string &punc_config, int thread_num)=0;
-	  virtual std::string AddPunc(const char* sz_input){};
-	  virtual std::string AddPunc(const char* sz_input, std::vector<std::string>& arr_cache){};
+	  virtual std::string AddPunc(const char* sz_input){return "";};
+	  virtual std::string AddPunc(const char* sz_input, std::vector<std::string>& arr_cache){return "";};
 };
 
 PuncModel *CreatePuncModel(std::map<std::string, std::string>& model_path, int thread_num, PUNC_TYPE type=PUNC_OFFLINE);
