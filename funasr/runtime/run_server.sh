054462013ab14cd52bb66c2110cbfbe333d1b0f6
wucong.lyb <wucong.lyb@alibaba-inc.com>
2023-06-30 11:49:47 +0800

    add run_server.sh

---
+++ funasr/runtime/run_server.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+download_model_dir="/workspace/models"
+model_dir="damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-onnx"
+vad_dir="damo/speech_fsmn_vad_zh-cn-16k-common-onnx"
+punc_dir="damo/punc_ct-transformer_zh-cn-common-vocab272727-onnx"
+decoder_thread_num=32
+io_thread_num=8
+port=10095
+certfile="../../../ssl_key/server.crt"
+keyfile="../../../ssl_key/server.key"
+
+. ../../../egs/aishell/transformer/utils/parse_options.sh || exit 1;
+
+cd /workspace/FunASR/funasr/runtime/websocket/build/bin
+./funasr-wss-server  \
+  --download-model-dir ${download_model_dir} \
+  --model-dir ${model_dir} \
+  --vad-dir ${vad_dir} \
+  --punc-dir ${punc_dir} \
+  --decoder-thread-num ${decoder_thread_num} \
+  --io-thread-num  ${io_thread_num} \
+  --port ${port} \
+  --certfile  ${certfile} \
+  --keyfile ${keyfile}
