0b7486343da2e4f195d8c42846c1b9b1a6ba5697
boji123 <boji123@aliyun.com>
2023-08-07 16:11:03 +0800

    [refator] optimize grpc server pipeline and instruction

--- funasr/runtime/grpc/run_server.sh
+++ funasr/runtime/grpc/run_server.sh
@@ -2,11 +2,11 @@
 
 ./build/bin/paraformer-server \
   --port-id 10100 \
-  --offline-model-dir /cfs/user/burkliu/data/funasr_models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-onnx \
-  --online-model-dir /cfs/user/burkliu/data/funasr_models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online \
+  --offline-model-dir funasr_models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-onnx \
+  --online-model-dir funasr_models/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online \
   --quantize true \
-  --vad-dir /cfs/user/burkliu/data/funasr_models/damo/speech_fsmn_vad_zh-cn-16k-common-onnx \
+  --vad-dir funasr_models/damo/speech_fsmn_vad_zh-cn-16k-common-onnx \
   --vad-quant true \
-  --punc-dir /cfs/user/burkliu/data/funasr_models/damo/punc_ct-transformer_zh-cn-common-vad_realtime-vocab272727 \
+  --punc-dir funasr_models/damo/punc_ct-transformer_zh-cn-common-vad_realtime-vocab272727 \
   --punc-quant true \
   2>&1
