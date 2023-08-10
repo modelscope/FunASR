e02062443b02c6627d4679b82b00aa5e1056f457
雾聪 <wucong.lyb@alibaba-inc.com>
2023-08-10 19:55:50 +0800

    update websocket-server-2pass

--- funasr/runtime/websocket/websocket-server-2pass.cpp
+++ funasr/runtime/websocket/websocket-server-2pass.cpp
@@ -180,7 +180,11 @@ void WebSocketServer::do_decoder(
 
 void WebSocketServer::on_open(websocketpp::connection_hdl hdl) {
   scoped_lock guard(m_lock);     // for threads safty
-  check_and_clean_connection();  // remove closed connection
+  try{
+    check_and_clean_connection();  // remove closed connection 
+  }catch (std::exception const& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
 
   std::shared_ptr<FUNASR_MESSAGE> data_msg =
       std::make_shared<FUNASR_MESSAGE>();  // put a new data vector for new
