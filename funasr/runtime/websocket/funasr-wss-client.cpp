63486ae2f3d1b761b0fae6fc7cbeaa36fa6ef9cd
雾聪 <wucong.lyb@alibaba-inc.com>
2023-08-16 20:11:43 +0800

    update funasr-wss-client

--- funasr/runtime/websocket/funasr-wss-client.cpp
+++ funasr/runtime/websocket/funasr-wss-client.cpp
@@ -321,7 +321,7 @@ class WebsocketClient {
         jsonresult["is_speaking"] = false;
         m_client.send(m_hdl, jsonresult.dump(), websocketpp::frame::opcode::text,
                       ec);
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     websocketpp::client<T> m_client;
 
