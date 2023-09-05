b8dd93aaada7500e85c7fe6ef8aa8935f9b1878c
zx <12345678>
2023-09-05 18:16:16 +0800

    Add paraformer-large decode lib and examples

---
+++ funasr/runtime/csharp/AliParaformerAsr/AliParaformerAsr.Examples.MauiApp/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        MainPage = new MySplashPage();
+        _ = EndSplash();
+    }
+    async Task EndSplash()
+    {
+        await Task.Delay(1000);
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            MainPage = new AppShell();
+        });
+    }
+}
---
+++ funasr/runtime/csharp/AliParaformerAsr/AliParaformerAsr/Utils/YamlHelper.cs
@@ -0,0 +1,34 @@
+﻿// See https://github.com/manyeyes for more information
+// Copyright (c)  2023 by manyeyes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using YamlDotNet.Serialization;
+
+namespace AliParaformerAsr.Utils
+{
+    /// <summary>
+    /// YamlHelper
+    /// Copyright (c)  2023 by manyeyes
+    /// </summary>
+    internal class YamlHelper
+    {
+        public static T ReadYaml<T>(string yamlFilePath)
+        {
+            if (!File.Exists(yamlFilePath))
+            {
+#pragma warning disable CS8603 // 可能返回 null 引用。
+                return default(T);
+#pragma warning restore CS8603 // 可能返回 null 引用。
+            }
+            StreamReader yamlReader = File.OpenText(yamlFilePath);
+            Deserializer yamlDeserializer = new Deserializer();
+            T info = yamlDeserializer.Deserialize<T>(yamlReader);
+            yamlReader.Close();
+            return info;
+        }
+    }
+}
