90eb1c1b545ec5276295bdf19df986a271466df7
andyweiqiu <andywei_89@163.com>
2023-09-05 18:25:28 +0800

    Delete redundant parts from an iOS project

---
+++ funasr/runtime/ios/paraformer_online/paraformer_online/SceneDelegate.h
@@ -0,0 +1,20 @@
+/**
+ * Copyright FunASR (https://github.com/alibaba-damo-academy/FunASR). All Rights
+ * Reserved. MIT License  (https://opensource.org/licenses/MIT)
+ */
+
+//
+//  SceneDelegate.h
+//  paraformer_online
+//
+//  Created by 邱威 on 2023/6/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
