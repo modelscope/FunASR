2165d5de05f48d4017fec319c117d859db3feaf0
haoneng.lhn <haoneng.lhn@alibaba-inc.com>
2023-09-12 13:00:16 +0800

    fix decoding_ind none bug

--- funasr/bin/asr_infer.py
+++ funasr/bin/asr_infer.py
@@ -430,9 +430,7 @@ class Speech2TextParaformer:
 
         # b. Forward Encoder
         if decoding_ind is None:
-            decoding_ind = self.decoding_ind
-        if self.decoding_ind is None:
-            decoding_ind = 0
+            decoding_ind = 0 if self.decoding_ind is None else self.decoding_ind
         enc, enc_len = self.asr_model.encode(**batch, ind=decoding_ind)
         if isinstance(enc, tuple):
             enc = enc[0]
