1f37a71f11288fb5bbb2dc29e12f4adaff022fd8
shixian.shi <shixian.shi@alibaba-inc.com>
2023-10-10 15:57:23 +0800

    bug fix

--- egs_modelscope/asr_vad_spk/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn/demo.py
+++ egs_modelscope/asr_vad_spk/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn/demo.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     inference_pipeline = pipeline(
         task=Tasks.auto_speech_recognition,
         model='damo/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn',
+        model_revision='v0.0.2',
         vad_model='damo/speech_fsmn_vad_zh-cn-16k-common-pytorch',
-        #punc_model='damo/punc_ct-transformer_zh-cn-common-vocab272727-pytorch',
         punc_model='damo/punc_ct-transformer_cn-en-common-vocab471067-large',
         output_dir=output_dir,
     )
