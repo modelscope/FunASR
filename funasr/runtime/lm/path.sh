988b8d31f9c6c74e66de6dc5c3731d50dca57eca
gongbo.gb <gongbo.gb@alibaba-inc.com>
2023-10-25 10:53:02 +0800

    Turn on fstbin compilation

--- funasr/runtime/lm/path.sh
+++ funasr/runtime/lm/path.sh
@@ -1,4 +1,3 @@
 export FUNASR_ROOT=`pwd`/../
 export PATH=$FUNASR_ROOT/onnxruntime/build/bin:$PWD:$PATH
-export PATH=/home/gongbo.gb/tools/python3/bin:/home/gongbo.gb/tools/SRILM-master/bin/i686-m64:$PWD:$PATH
 export LC_ALL=C
