50bff9241a93f3d6eb8fa8f7ca922c5b160fde33
xu <cdevelop@qq.com>
2023-11-24 23:29:36 +0800

    Adaptive 8K

--- runtime/onnxruntime/include/model.h
+++ runtime/onnxruntime/include/model.h
@@ -23,6 +23,8 @@ class Model {
     virtual void InitSegDict(const std::string &seg_dict_model){};
     virtual std::vector<std::vector<float>> CompileHotwordEmbedding(std::string &hotwords){return std::vector<std::vector<float>>();};
     virtual std::string GetLang(){return "";};
+    virtual int GetAsrSampleRate() = 0;
+
 };
 
 Model *CreateModel(std::map<std::string, std::string>& model_path, int thread_num=1, ASR_TYPE type=ASR_OFFLINE);
--- runtime/onnxruntime/src/fsmn-vad-online.h
+++ runtime/onnxruntime/src/fsmn-vad-online.h
@@ -21,6 +21,8 @@ public:
     std::vector<std::vector<int>> Infer(std::vector<float> &waves, bool input_finished);
     void ExtractFeats(float sample_rate, vector<vector<float>> &vad_feats, vector<float> &waves, bool input_finished);
     void Reset();
+    int GetVadSampleRate() { return vad_sample_rate_; };
+
     // 2pass
     std::unique_ptr<Audio> audio_handle = nullptr;
 
