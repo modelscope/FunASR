1159adbca076fa1a33bf4292ec5043e536285c5c
zhifu gao <zhifu.gzf@alibaba-inc.com>
2024-01-22 11:55:36 +0800

    funasr1.0 update (#1278)

--- examples/industrial_data_pretraining/bicif_paraformer/infer.sh
+++ examples/industrial_data_pretraining/bicif_paraformer/infer.sh
@@ -4,7 +4,7 @@ model_revision="v2.0.2"
 vad_model="damo/speech_fsmn_vad_zh-cn-16k-common-pytorch"
 vad_model_revision="v2.0.2"
 punc_model="damo/punc_ct-transformer_zh-cn-common-vocab272727-pytorch"
-punc_model_revision="v2.0.2"
+punc_model_revision="v2.0.3"
 spk_model="damo/speech_campplus_sv_zh-cn_16k-common"
 spk_model_revision="v2.0.2"
 
