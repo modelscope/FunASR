0a4e01bd7d789504cc5986fa848e5822bef4dfc9
语帆 <yf352572@alibaba-inc.com>
2024-02-28 17:18:23 +0800

    atsr

--- examples/industrial_data_pretraining/lcbnet/demo.sh
+++ examples/industrial_data_pretraining/lcbnet/demo.sh
@@ -1,6 +1,6 @@
 file_dir="/nfs/yufan.yf/workspace/github/FunASR/examples/industrial_data_pretraining/lcbnet/exp/speech_lcbnet_contextual_asr-en-16k-bpe-vocab5002-pytorch"
 
-CUDA_VISIBLE_DEVICES="" \
+#CUDA_VISIBLE_DEVICES="" \
 python -m funasr.bin.inference \
 --config-path=${file_dir} \
 --config-name="config.yaml" \
@@ -10,4 +10,4 @@ python -m funasr.bin.inference \
 +data_type='["kaldi_ark", "text"]' \
 ++tokenizer_conf.bpemodel=${file_dir}/bpe.model \
 ++output_dir="./outputs/debug" \
-++device="" \
+++device="cpu" \
---
+++ examples/industrial_data_pretraining/lcbnet/demo_nj.sh
@@ -0,0 +1,13 @@
+file_dir="/nfs/yufan.yf/workspace/github/FunASR/examples/industrial_data_pretraining/lcbnet/exp/speech_lcbnet_contextual_asr-en-16k-bpe-vocab5002-pytorch"
+
+#CUDA_VISIBLE_DEVICES="" \
+python -m funasr.bin.inference \
+--config-path=${file_dir} \
+--config-name="config.yaml" \
+++init_param=${file_dir}/model.pb \
+++tokenizer_conf.token_list=${file_dir}/tokens.txt \
+++input=[${file_dir}/wav.scp,${file_dir}/ocr.txt] \
++data_type='["kaldi_ark", "text"]' \
+++tokenizer_conf.bpemodel=${file_dir}/bpe.model \
+++output_dir="./outputs/debug" \
+++device="cpu" \
