d5c98e6f65cf4672e3a15f830733b85e616b9237
游雁 <zhifu.gzf@alibaba-inc.com>
2024-03-22 19:40:54 +0800

    update

--- examples/aishell/branchformer/run.sh
+++ examples/aishell/branchformer/run.sh
@@ -78,7 +78,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     --config-name "${config}" \
     ++train_data_set_list="${feats_dir}/data/${train_set}/audio_datasets.jsonl" \
     ++cmvn_file="${feats_dir}/data/${train_set}/cmvn.json" \
-    ++dataset_conf.num_workers=$nj
+
 fi
 
 token_list=${feats_dir}/data/${lang}_token_list/$token_type/tokens.txt
