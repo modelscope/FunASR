1df10311c4e6df252db748293fd47792abae6a61
游雁 <zhifu.gzf@alibaba-inc.com>
2024-03-25 10:16:51 +0800

    install requirements automatically

--- funasr/tokenizer/hf_tokenizer.py
+++ funasr/tokenizer/hf_tokenizer.py
@@ -1,15 +1,15 @@
 
-try:
-	from transformers import AutoTokenizer
-except:
-	# print("If you want to use hugging, please `pip install -U transformers`")
-	pass
+
 
 from funasr.register import tables
 
 @tables.register("tokenizer_classes", "HuggingfaceTokenizer")
 def HuggingfaceTokenizer(init_param_path, **kwargs):
-
+	try:
+		from transformers import AutoTokenizer
+	except:
+		# print("If you want to use hugging, please `pip install -U transformers`")
+		pass
 	tokenizer = AutoTokenizer.from_pretrained(init_param_path)
 	
 	return tokenizer
