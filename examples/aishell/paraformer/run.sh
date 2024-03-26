cc474910997e3f1f2229f9f959e5a91dfbbe3eae
游雁 <zhifu.gzf@alibaba-inc.com>
2024-03-26 12:54:12 +0800

    update

--- examples/aishell/paraformer/run.sh
+++ examples/aishell/paraformer/run.sh
@@ -76,7 +76,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     --config-path "${workspace}/conf" \
     --config-name "${config}" \
     ++train_data_set_list="${feats_dir}/data/${train_set}/audio_datasets.jsonl" \
-    ++cmvn_file="${feats_dir}/data/${train_set}/cmvn.json" \
+    ++cmvn_file="${feats_dir}/data/${train_set}/cmvn.json"
 fi
 
 token_list=${feats_dir}/data/${lang}_token_list/$token_type/tokens.txt
