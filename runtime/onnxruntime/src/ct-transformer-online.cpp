684023da59f624bead6c93228013450aff90b284
carl.che <carl.che@cloudminds.com>
2024-03-27 18:41:52 +0800

    <funasr>: <punc online>
    1.修正添加标点时英文首单词和第二个单词被错误合并的问题。

--- runtime/onnxruntime/src/ct-transformer-online.cpp
+++ runtime/onnxruntime/src/ct-transformer-online.cpp
@@ -122,7 +122,7 @@ string CTTransformerOnline::AddPunc(const char* sz_input, vector<string> &arr_ca
     {
         if (i > 0 && !(sentence_words_list[i][0] & 0x80) && (i + 1) < sentence_words_list.size() && !(sentence_words_list[i + 1][0] & 0x80))
         {
-            sentence_words_list[i] = sentence_words_list[i] + " ";
+            sentence_words_list[i] = " " + sentence_words_list[i];
         }
         if (nSkipNum < arr_cache.size())  //    if skip_num < len(cache):
             nSkipNum++;
