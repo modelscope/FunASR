fbaf234076ae6bed8e4484261f2cdb7a9ae696df
游雁 <zhifu.gzf@alibaba-inc.com>
2024-04-28 11:44:53 +0800

    batch

--- funasr/bin/train.py
+++ funasr/bin/train.py
@@ -13,7 +13,7 @@ from io import BytesIO
 
 from contextlib import nullcontext
 import torch.distributed as dist
-from collections.abc import Sequence
+
 from omegaconf import DictConfig, OmegaConf
 from torch.cuda.amp import autocast, GradScaler
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -99,7 +99,7 @@ def main(**kwargs):
     if freeze_param is not None:
         if "," in freeze_param:
             freeze_param = eval(freeze_param)
-        if not isinstance(freeze_param, Sequence):
+        if not isinstance(freeze_param, (list, tuple)):
             freeze_param = (freeze_param,)
         logging.info("freeze_param is not None: %s", freeze_param)
         for t in freeze_param:
