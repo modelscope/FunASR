3d269543bfd232a514ccfb5c382f235ebd5cb53f
游雁 <zhifu.gzf@alibaba-inc.com>
2024-05-15 16:37:58 +0800

    deepspeed

--- examples/industrial_data_pretraining/sense_voice/demo.py
+++ examples/industrial_data_pretraining/sense_voice/demo.py
@@ -7,8 +7,8 @@ from funasr import AutoModel
 
 model = AutoModel(
     model="/Users/zhifu/Downloads/modelscope_models/SenseVoiceModelscope",
-    vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
-    vad_kwargs={"max_single_segment_time": 30000},
+    # vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+    # vad_kwargs={"max_single_segment_time": 30000},
 )
 
 
