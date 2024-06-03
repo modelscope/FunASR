1c96d9aec2aecbce3191b1bcc375b79334067940
维石 <shixian.shi@alibaba-inc.com>
2024-06-03 15:20:37 +0800

    update demo

--- runtime/python/libtorch/demo_contextual_paraformer.py
+++ runtime/python/libtorch/demo_contextual_paraformer.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 from funasr_torch import Paraformer
 
-model_dir = "iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
-
+model_dir = "damo/speech_paraformer-large-contextual_asr_nat-zh-cn-16k-common-vocab8404"
 model = Paraformer(model_dir, batch_size=1)  # cpu
 # model = Paraformer(model_dir, batch_size=1, device_id=0)  # gpu
 
 wav_path = "{}/.cache/modelscope/hub/{}/example/asr_example.wav".format(Path.home(), model_dir)
+hotwords = "你的热词 魔搭"
 
-result = model(wav_path)
+result = model(wav_path, hotwords)
 print(result)
