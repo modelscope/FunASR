822345c2a47a60c7f78391907208e9b9a6fbebc9
雾聪 <wucong.lyb@alibaba-inc.com>
2024-06-12 13:59:09 +0800

    update funasr-onnx-offline-rtf

--- runtime/onnxruntime/bin/funasr-onnx-offline-rtf.cpp
+++ runtime/onnxruntime/bin/funasr-onnx-offline-rtf.cpp
@@ -259,6 +259,6 @@ int main(int argc, char *argv[])
     LOG(INFO) << "total_rtf " << (double)total_time/ (total_length*1000000);
     LOG(INFO) << "speedup " << 1.0/((double)total_time/ (total_length*1000000));
 
-    FunOfflineUninit(asr_handle);
+    // FunOfflineUninit(asr_handle);
     return 0;
 }
