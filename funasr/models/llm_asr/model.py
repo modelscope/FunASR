cc1a85962a6a697ae665f292f0abffad4abd7513
游雁 <zhifu.gzf@alibaba-inc.com>
2024-07-25 12:37:31 +0800

    bugfix python runtime

--- funasr/models/llm_asr/model.py
+++ funasr/models/llm_asr/model.py
@@ -958,7 +958,11 @@ class LLMASR4(nn.Module):
             src_state = torch.load(init_param_path, map_location="cpu")
             flag = audio_adaptor.load_state_dict(src_state, strict=False)
             logging.info(f"Loading audio_adaptor ckpt: {init_param_path}, status: {flag}")
-
+        freeze = audio_adaptor_conf.get("freeze", True)
+        if freeze:
+            for name, param in audio_adaptor.named_parameters():
+                param.requires_grad = False
+            audio_adaptor.eval()
         self.audio_adaptor = audio_adaptor
 
         self.error_calculator = None
