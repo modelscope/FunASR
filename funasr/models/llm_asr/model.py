17e64505e85bb6266dd3f0d5de2c275b6e111d12
游雁 <zhifu.gzf@alibaba-inc.com>
2024-07-25 15:28:18 +0800

    bugfix python runtime

--- funasr/models/llm_asr/model.py
+++ funasr/models/llm_asr/model.py
@@ -937,7 +937,10 @@ class LLMASR4(nn.Module):
 
         logging.info(f"use_lora: {llm_conf.get('use_lora', False)}")
         if llm_conf.get("use_lora", False):
+            from omegaconf import OmegaConf
+
             lora_conf = llm_conf.get("lora_conf", {})
+            lora_conf = OmegaConf.to_container(lora_conf, resolve=True)
             from peft import get_peft_model, LoraConfig, TaskType, PeftConfig, PeftModel
 
             lora_init_param_path = lora_conf.get("init_param_path", None)
