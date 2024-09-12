d87baad4326fb7e7f9707d7a585d5c8ea840e716
常材 <gaochangfeng.gcf@alibaba-inc.com>
2024-09-12 14:28:49 +0800

    fix 0length nbest

--- funasr/models/conformer_rhlf/model.py
+++ funasr/models/conformer_rhlf/model.py
@@ -39,14 +39,10 @@ class ConformerDPO(Conformer):
         self.beam_size = kwargs.get("beam_size", 10)
         self.mwer_rate = kwargs.get("mwer_rate", 0)
         char_list = [str(x) for x in range(self.vocab_size)]
-        
+
         self.ctc_decoder = cuda_ctc_decoder(
             char_list, nbest = self.nbest, beam_size = self.beam_size, blank_skip_threshold = 0.95)
-
-    def load_state_dict(self, state_dict, strict=True):
-        missing_keys_1, unexpected_keys_1 = super().load_state_dict(state_dict, strict)
-        missing_keys_2, unexpected_keys_2 = self.ref_model.load_state_dict(state_dict, strict)
-        return (missing_keys_1 + missing_keys_2, unexpected_keys_1 + unexpected_keys_2) 
+        
     def forward(
         self,
         speech: torch.Tensor,
@@ -70,7 +66,7 @@ class ConformerDPO(Conformer):
         batch_size = speech.shape[0]
 
         # 1. Encoder
-        encoder_out, encoder_out_lens, ref_encoder_out, ref_encoder_out_lens = self.encode(speech, speech_lengths)
+        encoder_out, encoder_out_lens = self.encode(speech, speech_lengths)
         intermediate_outs = None
         if isinstance(encoder_out, tuple):
             intermediate_outs = encoder_out[1]
@@ -125,41 +121,6 @@ class ConformerDPO(Conformer):
         loss, stats, weight = force_gatherable((loss, stats, batch_size), loss.device)
         return loss, stats, weight
 
-    def encode(
-        self,
-        speech: torch.Tensor,
-        speech_lengths: torch.Tensor,
-        **kwargs,
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        """Frontend + Encoder. Note that this method is used by asr_inference.py
-        Args:
-                speech: (Batch, Length, ...)
-                speech_lengths: (Batch, )
-                ind: int
-        """
-        with autocast(False):
-
-            # Data augmentation
-            if self.specaug is not None and self.training:
-                speech, speech_lengths = self.specaug(speech, speech_lengths)
-
-            # Normalization for feature: e.g. Global-CMVN, Utterance-CMVN
-            if self.normalize is not None:
-                speech, speech_lengths = self.normalize(speech, speech_lengths)
-
-        # Forward encoder
-        # feats: (Batch, Length, Dim)
-        # -> encoder_out: (Batch, Length2, Dim2)
-        encoder_out, encoder_out_lens, _ = self.encoder(speech, speech_lengths)
-        with torch.no_grad():
-            ref_encoder_out, ref_encoder_out_lens, _ = self.ref_model.encoder(speech, speech_lengths)
-
-        if isinstance(encoder_out, tuple):
-            intermediate_outs = encoder_out[1]
-            encoder_out = encoder_out[0]
-
-
-        return encoder_out, encoder_out_lens, ref_encoder_out, ref_encoder_out_lens
     def _calc_att_loss(
         self,
         encoder_out: torch.Tensor,
@@ -207,29 +168,31 @@ class ConformerDPO(Conformer):
 
     def _calc_dpo_loss(
         self,
-        encoder_out, 
+        encoder_out,
         encoder_out_lens,
         decoder_out,
-        ref_encoder_out, 
-        ys_pad, 
-        ys_pad_lens, 
-        n_best_tokens, 
-        n_best_probs, 
+        ref_encoder_out,
+        ys_pad,
+        ys_pad_lens,
+        n_best_tokens,
+        n_best_probs,
         n_best_length
     ):
         pass
 
     def _calc_mwer_loss(
         self,
-        encoder_out, 
+        encoder_out,
         encoder_out_lens,
         decoder_out,
-        ys_pad, 
-        ys_pad_lens, 
+        ys_pad,
+        ys_pad_lens,
 
     ):
-        n_best_tokens, n_best_ctc_probs, n_best_length = self._get_nbest(encoder_out, encoder_out_lens) 
+        n_best_tokens, n_best_ctc_probs, n_best_length = self._get_nbest(encoder_out, encoder_out_lens)
         nbest_dist = self._get_nbest_dist(ys_pad, ys_pad_lens, n_best_tokens, n_best_length)
+        if n_best_length.min() < 1: # 存在太短的nbest，不做mwer的计算
+            return torch.tensor(0.0).to(n_best_length.device)
         n_best_att_prob = self._get_nbest_attprob(encoder_out, encoder_out_lens, n_best_tokens, n_best_length)
         mwer_loss = torch.sum(nbest_dist * n_best_att_prob.softmax(dim=-1), dim = -1)
         return torch.mean(mwer_loss)
@@ -258,9 +221,8 @@ class ConformerDPO(Conformer):
         encoder_out = encoder_out.unsqueeze(1) # (B, 1, T, C)
         encoder_out = encoder_out.expand(-1, self.nbest,-1, -1).contiguous().view(-1, encoder_out.size(-2), encoder_out.size(-1))
         encoder_out_lens = encoder_out_lens.unsqueeze(1).expand(-1, self.nbest).contiguous().view(-1)
-        
         if n_best_tokens.nelement() == 0:
-            n_best_tokens = torch.empty(encoder_out.size(0) * self.nbest, 0).to(encoder_out.device)
+            n_best_tokens = torch.empty(encoder_out.size(0) * self.nbest, 0).to(encoder_out.device).long()
         else:
             n_best_tokens = n_best_tokens.view(-1, n_best_length.max()).contiguous()
         n_best_length = n_best_length.view(-1).contiguous()
@@ -281,16 +243,17 @@ class ConformerDPO(Conformer):
 
         return n_best_target_prob.view(-1, self.nbest)
 
+    @torch.no_grad()
     def _get_nbest_dist(self, ys_pad, ys_pad_lens, n_best_tokens, n_best_length):
         n_best_dist = torch.zeros(len(ys_pad), self.nbest)
         for i in range(len(n_best_dist)):
             for j in range(len(n_best_dist[i])):
                 n_best_dist[i,j] = editdistance.eval(
-                    ys_pad[i,:ys_pad_lens[i]], 
+                    ys_pad[i,:ys_pad_lens[i]],
                     n_best_tokens[i, j, :n_best_length[i, j]]
                 )
         return n_best_dist.to(ys_pad.device)
-    
+
     @staticmethod
     def get_batch_logps(
         logits: torch.FloatTensor,
@@ -324,4 +287,4 @@ class ConformerDPO(Conformer):
 
         per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
 
-        return (per_token_logps * loss_mask).sum(-1), loss_mask.sum(-1)
+        return (per_token_logps * loss_mask).sum(-1), loss_mask.sum(-1)     
