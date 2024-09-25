9c0bb5bf08db55a6148e457236d2b355cea9fe58
locasxe <71017860+locasxe@users.noreply.github.com>
2024-09-25 19:37:44 +0800

    onnxruntime memoryleak fix

--- runtime/onnxruntime/src/fsmn-vad.h
+++ runtime/onnxruntime/src/fsmn-vad.h
@@ -34,6 +34,7 @@ public:
     std::shared_ptr<Ort::Session> vad_session_ = nullptr;
     Ort::Env env_;
     Ort::SessionOptions session_options_;
+    Ort::AllocatorWithDefaultOptions vad_allocator;
     std::vector<const char *> vad_in_names_;
     std::vector<const char *> vad_out_names_;
     std::vector<std::vector<float>> in_cache_;
@@ -56,7 +57,8 @@ private:
 
     static void GetInputOutputInfo(
             const std::shared_ptr<Ort::Session> &session,
-            std::vector<const char *> *in_names, std::vector<const char *> *out_names);
+            std::vector<const char *> *in_names, std::vector<const char *> *out_names,
+            Ort::AllocatorWithDefaultOptions *allocator);
 
     void FbankKaldi(float sample_rate, std::vector<std::vector<float>> &vad_feats,
                     std::vector<float> &waves);
