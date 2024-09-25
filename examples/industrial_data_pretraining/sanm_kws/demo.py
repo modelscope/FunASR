467f51afc6cee9a736d30bd30c3a294009334f21
pengteng.spt <pengteng.spt@alibaba-inc.com>
2024-09-25 17:27:25 +0800

    add demo.py for all kws models

---
+++ examples/industrial_data_pretraining/sanm_kws/demo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Copyright FunASR (https://github.com/alibaba-damo-academy/FunASR). All Rights Reserved.
+#  MIT License  (https://opensource.org/licenses/MIT)
+
+from funasr import AutoModel
+
+model = AutoModel(
+    model="iic/speech_sanm_kws_phone-xiaoyun-commands-offline",
+    keywords="小云小云",
+    output_dir="./outputs/debug",
+    device='cpu'
+)
+
+test_wav = "https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/KWS/pos_testset/kws_xiaoyunxiaoyun.wav"
+
+res = model.generate(input=test_wav, cache={},)
+print(res)
