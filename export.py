2d4d0526b0c48897f96e7147f9600afbebcf0230
will_wang <53147925+willnufe@users.noreply.github.com>
2024-10-10 15:13:05 +0800

    paraformer-offline [fp32 fp16 onnx-gpu]

--- export.py
+++ export.py
@@ -7,4 +7,5 @@ model = AutoModel(
     model="/raid/t3cv/wangch/WORK_SAPCE/ASR/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
 )
 
-res = model.export(type="onnx", quantize=False, opset_version=13, device='cuda')
+res = model.export(type="onnx", quantize=False, opset_version=13, device='cuda')  # fp32 onnx-gpu
+# res = model.export(type="onnx_fp16", quantize=False, opset_version=13, device='cuda')  # fp16 onnx-gpu
