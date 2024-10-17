2a77cda1bcce8fabaeab516c2869f781c4395684
游雁 <zhifu.gzf@alibaba-inc.com>
2024-10-17 11:38:40 +0800

    MinMo_T2S

--- funasr/datasets/minmo_datasets/datasets.py
+++ funasr/datasets/minmo_datasets/datasets.py
@@ -1976,3 +1976,429 @@ class MinMo_T2S(torch.utils.data.Dataset):
             break
 
         return outputs
+
+
+@tables.register("dataset_classes", "MinMo_T2S")
+class MinMo_T2S(torch.utils.data.Dataset):
+    """ """
+
+    def __init__(
+        self,
+        path,
+        index_ds: str = None,
+        frontend=None,
+        tokenizer=None,
+        int_pad_value: int = -1,
+        float_pad_value: float = 0.0,
+        **kwargs,
+    ):
+        super().__init__()
+        index_ds_class = tables.index_ds_classes.get(index_ds)
+        self.index_ds = index_ds_class(path, **kwargs)
+        preprocessor_speech = kwargs.get("preprocessor_speech", None)
+        if preprocessor_speech:
+            preprocessor_speech_class = tables.preprocessor_classes.get(preprocessor_speech)
+            preprocessor_speech = preprocessor_speech_class(
+                **kwargs.get("preprocessor_speech_conf")
+            )
+        self.preprocessor_speech = preprocessor_speech
+        preprocessor_text = kwargs.get("preprocessor_text", None)
+        if preprocessor_text:
+            preprocessor_text_class = tables.preprocessor_classes.get(preprocessor_text)
+            preprocessor_text = preprocessor_text_class(**kwargs.get("preprocessor_text_conf"))
+        self.preprocessor_text = preprocessor_text
+
+        self.frontend = frontend
+        self.fs = 16000 if frontend is None else frontend.fs
+        self.data_type = "sound"
+        self.tokenizer = tokenizer
+
+        self.int_pad_value = int_pad_value
+        self.float_pad_value = float_pad_value
+        self.sos = kwargs.get("sos", "<|startoftranscript|>")
+        self.eos = kwargs.get("eos", "<|endoftext|>")
+        self.batch_size = kwargs.get("batch_size")
+        self.batch_type = kwargs.get("batch_type")
+        self.prompt_ids_len = 0
+        self.retry = kwargs.get("retry", 100)
+
+        self.permute = False
+        from funasr.frontends.whisper_frontend import WhisperFrontend
+
+        if isinstance(self.frontend, WhisperFrontend):
+            self.permute = True
+
+        self.pattern = re.compile(r"(<\|startofspeech\|>.*?<\|endofspeech\|>)")
+        # self.kwargs = kwargs
+        self.max_token_length = kwargs.get("max_token_length", 1500)
+        self.batch_size_scale_ratio_max = kwargs.get("batch_size_scale_ratio_max", 1.5)
+        self.batch_size_token_max = kwargs.get("batch_size_token_max", 2500)
+        self.multiturn_num_max = kwargs.get("multiturn_num_max", 5)
+        self.max_source_length = kwargs.get("max_source_length", 3000)
+
+    def get_source_len(self, index):
+        item = self.index_ds[index]
+        return self.index_ds.get_source_len(item)
+
+    def get_target_len(self, index):
+        item = self.index_ds[index]
+        return self.index_ds.get_target_len(item)
+
+    def __len__(self):
+        return len(self.index_ds)
+
+    def __getitem__(self, index):
+        # import pdb
+        #
+        # pdb.set_trace()
+
+        output = None
+
+        for idx in range(self.retry):
+            badcase_flag = False
+            if idx == 0:
+                index_cur = index
+            else:
+                index_cur = torch.randint(0, len(self.index_ds), ()).item()
+
+            item = self.index_ds[index_cur]
+
+            key = item.get("key", None)
+            system = item["system"]
+            user = item["user"]
+            assistant = item["assistant"]
+
+            (
+                input_ids,
+                labels,
+                fbank,
+                fbank_lens,
+                fbank_mask,
+                fbank_beg,
+                fake_token_len,
+                codec,
+                codec_len,
+                audio,
+                audio_len,
+                input_mask,
+                input_mask_beg,
+                spk_emb,
+                spk_emb_len,
+                codec_beg,
+                fake_codec_len,
+            ) = ([], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [])
+
+            multiturn_num = len(system)
+            for i, (system_prompt, user_prompt, target_out) in enumerate(
+                zip(system, user, assistant)
+            ):
+                if i >= self.multiturn_num_max:
+                    break
+                if len(input_ids) > self.max_token_length:
+                    logging.info(
+                        f"input_ids > max_token_length: {len(input_ids)}>{self.max_token_length}, {item}"
+                    )
+                    break
+
+                if i == 0:
+                    source_input = f"<|im_start|>system\n{system_prompt}<|im_end|>\n<|im_start|>user\n{user_prompt}<|im_end|>\n<|im_start|>assistant\n"
+                else:
+                    source_input = (
+                        f"<|im_start|>user\n{user_prompt}<|im_end|>\n<|im_start|>assistant\n"
+                    )
+
+                # inputs
+                splits = self.pattern.split(source_input)
+                source_ids = []
+                fbank_i = []
+                fbank_mask_i = []
+                fake_token_len_i = 0
+                fbank_beg_i = -1
+                fbank_lens_i = []
+                speech = []
+                speech_lengths = []
+                input_mask_i = []
+                target_ids = []
+                for k, sub_str in enumerate(splits):
+                    if not sub_str.startswith("<|startofspeech|>"):
+                        sub_token = self.tokenizer.encode(sub_str)
+                        source_ids += sub_token
+                        fbank_mask_i += [0] * len(sub_token)
+                    else:
+                        sub_str = sub_str.replace("<|startofspeech|>", "").replace(
+                            "<|endofspeech|>", ""
+                        )
+                        if sub_str.startswith("!"):
+                            try:
+                                data_src = load_audio_text_image_video(sub_str[1:], fs=self.fs)
+                            except Exception as e:
+                                logging.error(
+                                    f"Loading wav failed! {str(e)}, {traceback.format_exc()}"
+                                )
+                                badcase_flag = True
+                                continue
+                            speech, speech_lengths = extract_fbank(
+                                data_src,
+                                data_type=self.data_type,
+                                frontend=self.frontend,
+                                is_final=True,
+                            )  # speech: [b, T, d]
+                            if speech_lengths > self.max_source_length:
+                                logging.info(
+                                    f"speech_lengths > max_source_length: {speech_lengths}>{self.max_source_length}, {item}"
+                                )
+                                badcase_flag = True
+                            if self.permute:
+                                speech = speech.permute(0, 2, 1)
+                            # if speech_lengths > self.batch_size:
+                            #     continue
+
+                            olens = 1 + (speech_lengths[0].item() - 3 + 2 * 1) // 2
+                            olens = 1 + (olens - 3 + 2 * 1) // 2
+                            fake_token_len_i = (olens - 1) // 2 + 1
+                            fake_token = [0] * fake_token_len_i
+                            fbank_beg_i = len(source_ids)
+                            source_ids += fake_token
+                            fbank_mask_i += [1] * len(fake_token)
+
+                if badcase_flag:
+                    continue
+
+                # targets
+
+                meta = None
+                if isinstance(target_out, (list, tuple)):
+                    target_out, meta = target_out
+                    if isinstance(meta, dict):
+                        if "wav_path" in meta:
+                            wav_path = meta["wav_path"]
+                            wav_samples = load_audio_text_image_video(wav_path, fs=22050)
+                            audio.append(wav_samples)
+                            audio_len.append(len(wav_samples))
+                        if "spk_emb":
+                            spk_emb_path = meta["spk_emb"]
+                            sub_spk_emb = np.load(spk_emb_path)
+                            spk_emb.append(sub_spk_emb)
+                            spk_emb_len.append(len(sub_spk_emb))
+
+                target_out = f"{target_out}<|im_end|>"
+                splits = self.pattern.split(target_out)
+                codec_i = []
+                sub_token = []
+                codec_beg_i = ([],)
+                fake_codec_len_i = ([],)
+                for k, sub_str in enumerate(splits):
+
+                    if not sub_str.startswith("<|startofspeech|>"):
+                        # sub_str = f"{sub_str}<|im_end|>"
+                        sub_token = self.tokenizer.encode(sub_str)
+                        target_ids += sub_token
+                    else:
+                        sub_str = sub_str.replace("<|startofspeech|>", "").replace(
+                            "<|endofspeech|>", ""
+                        )
+                        if sub_str.startswith("!"):
+                            sub_token_codec = np.load(sub_str[1:])
+                            codec_i = torch.from_numpy(sub_token_codec, dtype=torch.int64)
+                            codec_i_len = len(sub_token_codec)
+                            fake_token = [0] * codec_i_len
+                            target_ids += fake_token
+
+                if len(codec_i) > 0:
+                    codec.append(codec_i)
+                    codec_len.append(codec_i_len)
+                fbank_beg += [fbank_beg_i + len(input_ids)]
+                fake_token_len += [fake_token_len_i]
+                source_mask = [-100] * len(source_ids)
+
+                codec_beg_i = len(source_ids)
+                fake_codec_len_i = len(codec_i)
+                codec_beg += [codec_beg_i + len(input_ids)]
+                fake_codec_len += [fake_codec_len_i]
+
+                input_ids += source_ids + target_ids
+                labels += source_mask + target_ids
+
+                fbank_mask += fbank_mask_i
+                if len(speech) > 0:
+                    fbank.append(speech[0, :, :])
+                    fbank_lens.append(speech_lengths)
+
+            if badcase_flag:
+                continue
+
+            input_ids = torch.tensor(input_ids, dtype=torch.int64)  # [: self.max_token_length]
+            attention_mask = torch.tensor([1] * len(input_ids), dtype=torch.int32)
+            labels = torch.tensor(labels, dtype=torch.int64)  # [: self.max_token_length]
+
+            # fbank = speech[0, :, :]
+            # fbank_lens = torch.tensor(fbank_lens, dtype=torch.int32)
+            fbank_mask = torch.tensor(fbank_mask, dtype=torch.float32)
+            fbank_beg = torch.tensor(fbank_beg, dtype=torch.int32)
+            fake_token_len = torch.tensor(fake_token_len, dtype=torch.int32)
+
+            output = {
+                "fbank_mask": fbank_mask,
+                "fbank_beg": fbank_beg,
+                "fake_token_len": fake_token_len,
+                "input_ids": input_ids,
+                "attention_mask": attention_mask,
+                "labels_ids": labels,
+            }
+            if len(codec) > 0:
+                codec_len = torch.tensor(codec_len, dtype=torch.int32)
+                codec_beg = torch.tensor(codec_beg, dtype=torch.int32)
+                fake_codec_len = torch.tensor(fake_codec_len, dtype=torch.int32)
+                output["codec"] = codec
+                output["codec_len"] = codec_len
+                output["codec_beg"] = codec_beg
+                output["fake_codec_len"] = fake_codec_len
+
+            if len(fbank) > 0:
+                output["speech"] = fbank
+                output["speech_lengths"] = fbank_lens
+            if len(audio) > 0:
+                output["audio"] = audio
+                output["audio_len"] = audio_len  # torch.tensor(audio_len, dtype=torch.int32)
+            # if len(input_mask) > 0:
+            #     output["input_mask"] = input_mask
+            #     output["input_mask_beg"] = input_mask_beg
+
+            if key is not None:
+                output["key"] = key
+            break
+
+        return output
+
+    def collator(self, samples: list = None):
+
+        for idx in range(self.retry):
+            badcase_flag = False
+
+            outputs = {}
+            for sample in samples:
+                if sample is None:
+                    continue
+                for key in sample.keys():
+                    if key not in outputs:
+                        outputs[key] = []
+                    if isinstance(sample[key], (list, tuple)):
+                        outputs[key].extend(sample[key])
+                    else:
+                        outputs[key].append(sample[key])
+
+            for key, data_list in outputs.items():
+                if isinstance(data_list[0], torch.Tensor):
+                    if data_list[0].dtype == torch.int64 or data_list[0].dtype == torch.int32:
+
+                        pad_value = self.int_pad_value
+                    else:
+                        pad_value = self.float_pad_value
+
+                    outputs[key] = torch.nn.utils.rnn.pad_sequence(
+                        data_list, batch_first=True, padding_value=pad_value
+                    )
+
+            if self.batch_type != "example":
+                b, t = outputs["input_ids"].shape
+                if b > 1 and b * t > self.batch_size_token_max:
+                    logging.info(
+                        f"Warning, {idx}th, b*t: {b}*{t}={b * t} > batch_size_sample_max: {self.batch_size_token_max}, drop last data"
+                    )
+                    samples = samples[:-1]
+                    continue
+
+            break
+
+        return outputs
+
+
+import hydra
+import logging
+from omegaconf import DictConfig, OmegaConf
+
+
+@hydra.main(config_name=None, version_base=None)
+def main_hydra(kwargs: DictConfig):
+    if kwargs.get("debug", False):
+        import pdb
+
+        pdb.set_trace()
+
+    from index_ds import OpenAIIndexDSJsonlForFullDuplexVAD
+
+    index_ds = OpenAIIndexDSJsonlForFullDuplexVAD(
+        path="/mnt/workspace/qinglin.zql/project/datasets/gpt-4o/vad/test/vad_test_samples.jsonl"
+    )
+    print(index_ds.contents)
+
+    kwargs = OmegaConf.to_container(kwargs, resolve=True)
+    # build frontend if frontend is none None
+    frontend = kwargs.get("frontend", None)
+    if frontend is not None:
+        frontend_class = tables.frontend_classes.get(frontend)
+        frontend = frontend_class(**kwargs["frontend_conf"])
+        kwargs["frontend"] = frontend
+        kwargs["input_size"] = frontend.output_size()
+
+        # build tokenizer
+        tokenizer = kwargs.get("tokenizer", None)
+        if tokenizer is not None:
+            tokenizer_class = tables.tokenizer_classes.get(tokenizer)
+            tokenizer = tokenizer_class(**kwargs.get("tokenizer_conf", {}))
+            kwargs["token_list"] = (
+                tokenizer.token_list if hasattr(tokenizer, "token_list") else None
+            )
+            kwargs["token_list"] = (
+                tokenizer.get_vocab() if hasattr(tokenizer, "get_vocab") else kwargs["token_list"]
+            )
+            vocab_size = len(kwargs["token_list"]) if kwargs["token_list"] is not None else -1
+            if vocab_size == -1 and hasattr(tokenizer, "get_vocab_size"):
+                vocab_size = tokenizer.get_vocab_size()
+        else:
+            vocab_size = -1
+        kwargs["tokenizer"] = tokenizer
+
+    # dataset
+    dataset_class = tables.dataset_classes.get(kwargs.get("dataset", "AudioDataset"))
+    dataset_train = dataset_class(
+        kwargs.get("train_data_set_list"),
+        frontend=frontend,
+        tokenizer=tokenizer,
+        is_training=False,
+        **kwargs.get("dataset_conf"),
+    )
+
+    # dataloader
+    batch_sampler = kwargs["dataset_conf"].get("batch_sampler", "BatchSampler")
+    batch_sampler_class = tables.batch_sampler_classes.get(batch_sampler)
+    dataset_conf = kwargs.get("dataset_conf")
+    print(f"dataset_conf:\n{dataset_conf}")
+    batch_sampler_train = batch_sampler_class(dataset_train, is_training=False, **dataset_conf)
+
+    dataloader_train = torch.utils.data.DataLoader(
+        dataset_train, collate_fn=dataset_train.collator, **batch_sampler_train
+    )
+
+    for batch_idx, batch in enumerate(dataloader_train):
+        print(batch_idx)
+        for key in batch.keys():
+            print(key)
+            print(batch[key].size())
+            if key != "speech":
+                print(batch[key].tolist())
+
+        break
+
+
+if __name__ == "__main__":
+    main_hydra()
+    """
+    local_path="/mnt/workspace/qinglin.zql/project/datasets/gpt-4o/vad"
+    config="Speech2Text_Align_val_exp3_last8.yaml"
+
+    python funasr/datasets/openai_datasets/datasets.py \
+    --config-path "${local_path}" \
+    --config-name "${config}" \
+    ++train_data_set_list="/mnt/workspace/qinglin.zql/project/datasets/gpt-4o/vad/test/vad_test_samples.jsonl"
+    """
