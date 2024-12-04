893f4b935bbc81ad05f93d9603abe92a9acb6306
zhifu gao <zhifu.gzf@alibaba-inc.com>
2024-12-04 17:47:12 +0800

    Update export.py

--- export.py
+++ export.py
@@ -1,7 +1,5 @@
 # method2, inference from local path
 from funasr import AutoModel
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '4'
 
 model = AutoModel(
     model="/raid/t3cv/wangch/WORK_SAPCE/ASR/models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
