modelscope/FunASR

commit c1e365fea09aafda387cac12fdff43d28c598979

fix: resolve unexpected 'out of memory' issue in multi-GPU setup (#2373)
Fixed a bug where calling torch.cuda.empty_cache() caused extra memory usage on 'cuda:0', leading to unexpected 'out of memory' errors in multi-GPU environments.

Reference:
- https://github.com/pytorch/pytorch/issues/25752
- https://github.com/pytorch/pytorch/issues/144025

funasr/auto/auto_model.py
@@ -366,7 +366,8 @@ class AutoModel:
         if pbar:
             # pbar.update(1)
             pbar.set_description(f"rtf_avg: {time_escape_total/time_speech_total:0.3f}")
-        torch.cuda.empty_cache()
+        with torch.cuda.device(next(model.parameters()).device):
+            torch.cuda.empty_cache()
         return asr_result_list
 
     def inference_with_vad(self, input, input_len=None, **cfg):

funasr/bin/train.py
@@ -221,7 +221,8 @@ def main(**kwargs):
             )
             trainer.start_step = 0
 
-            torch.cuda.empty_cache()
+            with torch.cuda.device(kwargs["device"]):
+                torch.cuda.empty_cache()
 
             time_escaped = (time.perf_counter() - time_slice_i) / 3600.0
             logging.info(

funasr/bin/train_ds.py
@@ -184,7 +184,8 @@ def main(**kwargs):
             )
             trainer.start_step = 0
 
-            torch.cuda.empty_cache()
+            with torch.cuda.device(kwargs["device"]):
+                torch.cuda.empty_cache()
 
             time_escaped = (time.perf_counter() - time_slice_i) / 3600.0
             logging.info(

funasr/models/language_model/rnn/decoders.py
@@ -873,7 +873,8 @@ class Decoder(torch.nn.Module, ScorerInterface):
                         ctc_state[idx], accum_best_ids
                     )
 
-        torch.cuda.empty_cache()
+        with torch.cuda.device(vscores.device):
+            torch.cuda.empty_cache()
 
         dummy_hyps = [{"yseq": [self.sos, self.eos], "score": np.array([-float("inf")])}]
         ended_hyps = [

commit ce73d9cb485bdd4572eef65e6c4497ecb0afa4c8

fix: resolve CPU runtime error introduced by previous commit (c1e365f)
Fixed a bug that caused a runtime error when running the model on CPU, which was introduced in commit c1e365fea09aafda387cac12fdff43d28c598979. The error was related to incorrect handling of device placement.

funasr/auto/auto_model.py
@@ -366,8 +366,11 @@ class AutoModel:
         if pbar:
             # pbar.update(1)
             pbar.set_description(f"rtf_avg: {time_escape_total/time_speech_total:0.3f}")
-        with torch.cuda.device(next(model.parameters()).device):
-            torch.cuda.empty_cache()
+
+        device = next(model.parameters()).device
+        if device.type == 'cuda':
+            with torch.cuda.device():
+                torch.cuda.empty_cache()
         return asr_result_list
 
     def inference_with_vad(self, input, input_len=None, **cfg):

funasr/bin/train.py
@@ -221,8 +221,10 @@ def main(**kwargs):
             )
             trainer.start_step = 0
 
-            with torch.cuda.device(kwargs["device"]):
-                torch.cuda.empty_cache()
+            device = next(model.parameters()).device
+            if device.type == 'cuda':
+                with torch.cuda.device():
+                    torch.cuda.empty_cache()
 
             time_escaped = (time.perf_counter() - time_slice_i) / 3600.0
             logging.info(

funasr/bin/train_ds.py
@@ -184,8 +184,10 @@ def main(**kwargs):
             )
             trainer.start_step = 0
 
-            with torch.cuda.device(kwargs["device"]):
-                torch.cuda.empty_cache()
+            device = next(model.parameters()).device
+            if device.type == 'cuda':
+                with torch.cuda.device():
+                    torch.cuda.empty_cache()
 
             time_escaped = (time.perf_counter() - time_slice_i) / 3600.0
             logging.info(

funasr/models/language_model/rnn/decoders.py
@@ -873,8 +873,10 @@ class Decoder(torch.nn.Module, ScorerInterface):
                         ctc_state[idx], accum_best_ids
                     )
 
-        with torch.cuda.device(vscores.device):
-            torch.cuda.empty_cache()
+        device = vscores.device
+        if device.type == 'cuda':
+            with torch.cuda.device():
+                torch.cuda.empty_cache()
 
         dummy_hyps = [{"yseq": [self.sos, self.eos], "score": np.array([-float("inf")])}]
         ended_hyps = [