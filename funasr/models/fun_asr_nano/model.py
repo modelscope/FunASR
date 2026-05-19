89ef5f7ae9df61c4a19c5967e21755d3435291c5
游雁 <zhifu.gzf@alibaba-inc.com>
2026-05-19 17:53:56 +0000

    fix: Fun-ASR-Nano batch check uses actual data size instead of kwargs batch_size
    - The batch_size in kwargs is set by inference_with_vad for batching segments,
      not for the model itself. Check len(data_in) > 1 instead.

--- funasr/models/fun_asr_nano/model.py
+++ funasr/models/fun_asr_nano/model.py
@@ -469,7 +469,7 @@ class FunASRNano(nn.Module):
     ):
         meta_data = {}
 
-        if kwargs.get("batch_size", 1) > 1:
+        if len(data_in) > 1:
             raise NotImplementedError("batch decoding is not implemented")
 
         contents = self.data_template(data_in[0])
